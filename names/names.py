ba6b0f28a53e0e04013de2731625b029a8180fb2
Dom Eccleston <domeccleston@gmail.com>
2020-02-28 10:36:28 +0000

    reverse

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -11,16 +12,45 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
+duplicates2 = {}
+# base: this is O(n^2)
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# mvp
+
+# bst = BinarySearchTree(names_1[0])
+# count = 0
+# for name in names_1:
+#     if count == 0:
+#         count += 1
+#     else:
+#         bst.insert(name)
+
+# for name in names_2:
+#     if bst.contains(name):
+#         duplicates.append(name)
+
+# stretch: O(n)
+
+
+for name in names_1:
+    duplicates2[name] = 1
+
+for name in names_2:
+    if name in duplicates2:
+        duplicates2[name] += 1
+
+for k, v in duplicates2.items():
+    if v > 1:
+        duplicates.append(k)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,31 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+        else:
+            if self.current is self.storage.head:
+                self.storage.delete(self.storage.tail)
+                self.storage.add_to_tail(item)
+                self.current = self.storage.tail
+            else:
+                to_delete = self.current.prev
+                self.storage.delete(to_delete)
+                self.current.insert_before(item)
+                if self.current == self.storage.head:
+                    self.storage.head = self.storage.head.prev
+                print(self.storage.head.value)
+                self.current = self.current.prev
+                self.storage.length += 1
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-
+        i = self.storage.tail
+        while i is not None:
+            list_buffer_contents.append(i.value)
+            i = i.prev
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
@@ -23,7 +40,7 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
 
     def append(self, item):
         pass
